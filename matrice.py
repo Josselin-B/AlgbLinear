5f45a64dafce1b5a673606360ed4f97c82017273
Josselin-B <josselin2808@gmail.com>
2024-06-11 17:27:57 +0200

    Ajout d'une fonction pour additionner des matrice + différent test

--- matrice.py
+++ matrice.py
@@ -13,8 +13,9 @@ class Matrice:
     def shape(self):
         return (len(self.vecteurs), len(self.vecteurs[0].elements))
 
-    #def addition(self, autre_matrice):
-
+    def addition(self, autre_matrice):
+        result = [self.vecteurs[i].addition(autre_matrice.vecteurs[i]) for i in range(len(self.vecteurs))]
+        return Matrice(result)
 
     #def produit_matrice(self, autre_matrice):
 
@@ -24,3 +25,14 @@ class Matrice:
 
     def __repr__(self):
         return f"Matrice([\n" + ",\n".join([str(v) for v in self.vecteurs]) + "\n])"
+
+vec1 = vecteur([1, 2])
+vec2 = vecteur([3, 4])
+vec3 = vecteur([5, 6])
+vec4 = vecteur([7, 8])
+
+mat1 = Matrice([vec1, vec2])
+mat2 = Matrice([vec2, vec3])
+print(mat1)
+print(mat2)
+print(mat1.addition(mat2))
